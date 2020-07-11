91dbf751a7931e042deab36565e88e43c0a60d59
Ro <sealthepirate@gmail.com>
2020-07-11 13:52:02 +0300

    18 added new requirements , fixed blank lines from common and locators dir's

--- common/Login_Constants.py
+++ common/Login_Constants.py
@@ -1,6 +1,7 @@
 """Constants for Authorization."""
 
+
 class AutorizedUser:
-    AUTH_USER='Mari Mari'
-    AUTH_EMAIL_ALERT_TEXT='There is 1 error\nAn email address required.'
-    AUTH_PASS_ALERT_TEXT='There is 1 error\nInvalid password.'
+    AUTH_USER = 'Mari Mari'
+    AUTH_EMAIL_ALERT_TEXT = 'There is 1 error\nAn email address required.'
+    AUTH_PASS_ALERT_TEXT = 'There is 1 error\nInvalid password.'
