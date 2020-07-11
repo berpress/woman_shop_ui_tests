e9a3e2e26849df65b242b663665a514e152a3033
test1910md <test1910md@yandex.ru>
2020-07-11 07:29:23 +0500

    Authirization test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,4 @@
+from common.Login_Constants import AutorizedUser
 from model.login import UserData
 
 
@@ -5,14 +6,42 @@ def test_auth_shop(app):
     """
     Шаги
     1. Открываем главную страницу
-    2. Нажимаем кнопкн логин
+    2. Нажимаем кнопку логин
     3. Вводим вводим валидные данные
-    4. Проверяем имя пользователя
+    4. Проверяем данные авторизации
     """
     app.open_main_page()
-    user_data = UserData(login=None, password='password')
+    user_data = UserData(login='test1910md@mail.ru', password='191089')
     app.login.auth(user_data)
-    assert True
+    assert app.login.login_button_get_text() == AutorizedUser.AUTH_USER
+
+
+def test_unsuccess_email_auth(app):
+    """
+    Шаги
+    1. Открываем главную страницу
+    2. Нажимаем кнопку логин
+    3. Вводим вводим пустой логин
+    4. Проверяем данные авторизации
+    """
+    app.open_main_page()
+    user_data = UserData(login=None, password='191089')
+    app.login.auth(user_data)
+    assert app.login.login_auth_alert_get_text() == AutorizedUser.AUTH_EMAIL_ALERT_TEXT
+
+
+def test_unsuccess_password_auth(app):
+    """
+    Шаги
+    1. Открываем главную страницу
+    2. Нажимаем кнопку логин
+    3. Вводим вводим пустой пароль
+    4. Проверяем данные авторизации
+    """
+    app.open_main_page()
+    user_data = UserData(login='test1910md@mail.ru', password=None)
+    app.login.auth(user_data)
+    assert app.login.login_auth_alert_get_text() == AutorizedUser.AUTH_PASS_ALERT_TEXT
 
 
 def test_auth(login):
