fe7a6a2fb9ab886b1a1db6af1c1ba510a625ad91
orchi <orchid345617@gmail.com>
2020-07-21 10:11:38 +0300

    fix

---
+++ model/contact_us.py
@@ -0,0 +1,5 @@
+class ContactUsUserData:
+    def __init__(self, email: str, text: str, text_in_form: str):
+        self.email = email
+        self.text = text
+        self.text_in_form = text_in_form
