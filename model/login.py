11fdfeb083d0ebe160fc7eccd44af65d37ef929c
orchi <orchid345617@gmail.com>
2020-07-26 11:18:07 +0300

    fix reqs

--- model/login.py
+++ model/login.py
@@ -1,5 +1,13 @@
+import faker
+
+
 class UserData:
-    def __init__(self, login, password):
+    def __init__(self, login=None, password=None):
         self.login = login
         self.password = password
 
+    @staticmethod
+    def random_user():
+        """Функция генерации фейковых данных."""
+        fake = faker.Faker()
+        return UserData(login=fake.email(), password=fake.password())
